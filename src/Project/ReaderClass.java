720e6fe3ab416682f283a24602048e2919a65fc5
Lucky-05 <99110335+Lucky-05@users.noreply.github.com>
2023-04-12 08:19:30 -0600

    ReaderClass commit

--- src/Project/ReaderClass.java
+++ src/Project/ReaderClass.java
@@ -4,17 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class ReaderClass {
+	public static final int columns = 3; 
 	
 	public static String ReadArchive(String route) {
 		File file = new File(route);
 		String result = ""; 
+		int counter = 0;
+		String[] temporal;
+		String[][] user;
 		
+		
+
 		try (BufferedReader buffered = new BufferedReader(new FileReader(file))){
 			String line; 
 			
 			while((line = buffered.readLine()) != null) {
-				result = line+ "\n";
+				counter ++;
 			}
+			
+			user = new String[3][counter];
+			
+			for(int i = 0; i<counter; i++) {
+				line = buffered.readLine();
+				 temporal = line.split(" ");
+				for(int j = 0; j< temporal.length; j++) {
+					 user[j][i] = temporal[j];
+					
+				}
+			}
+			
+			
+			buffered.close();
+			
 		}catch(IOException e){
 			e.printStackTrace();
 		}
